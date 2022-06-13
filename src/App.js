38d52e4eee5e3e9eb388f69c1f2b5cd00e282601
hdcodesdev <91739271+hdcodesdev@users.noreply.github.com>
2022-06-13 23:11:56 +0100

    BEM example

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import './App.css';
 
-function App() {
-  const name = 'sunny';
-  const isMale = true;
+// BEM - block element modified
+//block__element--modifier
 
-  function getGreeting(user) {
-    if (user) {
-      return <h1>Hello, {user}!</h1>;
-    }
-    return <h1>Hello, Stranger.</h1>;
-  }
+function App() {
 
   return (
     <div className="App">
-      <h1>Hello {name}</h1>
-      <h1>{getGreeting()}</h1>
-      <h2>You are a {isMale ? 'male' : 'female'}</h2>
+      <h1 className="app__title">This is a title</h1>
+      <h1 className="app__title app__title--large">This is a large title</h1>
+      <h1 className="app__title app__title--small">This is a small title</h1>
     </div>
   );
 }
