cf782963733ddba451904600211735513ad0c82b
hdcodesdev <91739271+hdcodesdev@users.noreply.github.com>
2022-06-13 23:24:53 +0100

    class based component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import './App.css';
-import Header from './Header';
+// import './App.css';
+// import Header from './Header';
 
-// BEM - block element modified
-//block__element--modifier
+// // BEM - block element modified
+// //block__element--modifier
 
-function App() {
+// // functional component - function that returns something
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <h1 className="app__title">This is a title</h1>
+//     </div>
+//   );
+// }
 
-  return (
-    <div className="App">
-      <Header />
-      <h1 className="app__title">This is a title</h1>
-    </div>
-  );
+// export default App;
+
+
+import React, { Component } from 'react'
+// class based:
+class App extends Component {
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
 }
-
-export default App;
