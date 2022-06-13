191642e93a79278008d8e5c0843d75ba3529a23d
hdcodesdev <91739271+hdcodesdev@users.noreply.github.com>
2022-06-13 23:05:13 +0100

    jsx play around + cleanup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 
 function App() {
+  const name = 'sunny';
+  const isMale = true;
+
+  function getGreeting(user) {
+    if (user) {
+      return <h1>Hello, {user}!</h1>;
+    }
+    return <h1>Hello, Stranger.</h1>;
+  }
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>Hello {name}</h1>
+      <h1>{getGreeting()}</h1>
+      <h2>You are a {isMale ? 'male' : 'female'}</h2>
     </div>
   );
 }
