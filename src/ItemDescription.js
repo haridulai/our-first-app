3d4a8b5912405043141b5da02f75abb41c33bd0f
hdcodesdev <91739271+hdcodesdev@users.noreply.github.com>
2022-06-14 08:38:03 +0100

    converting to class based component

--- src/ItemDescription.js
+++ src/ItemDescription.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 
-function ItemDescription({ name, description }) {
-  return (
-    <div>
-      <p>{name}</p>
-      <p>
-        <i>{description}</i>
-      </p>
-    </div>
-  );
+export default class ItemDescription extends Component {
+  render() {
+    const { name, description } = this.props;
+    return (
+      <div>
+        <p>{name}</p>
+        <p>
+          <i>{description}</i>
+        </p>
+      </div>
+    );
+  }
 }
-
-export default ItemDescription;
